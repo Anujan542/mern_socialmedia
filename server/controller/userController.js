03f7b631c9877dc2c3ceb0ebe25022da8a0be1cf
anujan <anujan9300@gmail.com>
2021-05-08 08:05:59 +0530

    New Commit

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -24,3 +24,35 @@ exports.getUser = asyncHandler(async (req, res) => {
     res.status(400).json({ message: "User not found" });
   }
 });
+
+//update user profile
+exports.updateProfile = asyncHandler(async (req, res) => {
+  const {
+    avatar,
+    fullname,
+    mobile,
+    address,
+    website,
+    story,
+    gender,
+  } = req.body;
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    {
+      avatar,
+      fullname,
+      mobile,
+      address,
+      website,
+      story,
+      gender,
+    }
+  );
+
+  if (user) {
+    res.status(200).json({ user });
+  } else {
+    res.status(400).json({ meesage: "Something wrong!" });
+  }
+});
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { searchUser, getUser } = require("../controller/userController");
+const {
+  searchUser,
+  getUser,
+  updateProfile,
+} = require("../controller/userController");
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/search").get(protect, searchUser);
 router.route("/user/:id").get(protect, getUser);
+router.route("/update").patch(protect, updateProfile);
 
 module.exports = router;
